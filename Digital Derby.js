1bb831f4a976d8b63cd92f9e304cf4e77388197d
mjkiyoi <53839595+mjkiyoi@users.noreply.github.com>
2019-08-06 14:50:51 -0700

    Update Digital Derby.js

--- Digital Derby.js
+++ Digital Derby.js
@@ -1,11 +1,16 @@
-function startRace(){
+		  function pickHorse(){
+		       //collects user imput for game
+		       let usersHorse = prompt("What NUMBER Horse", "Enter a number from 1 - 6");
+		       //records the number the user imputed
+		       document.getElementById('choice').innerText = usersHorse;		
+		}
+			function startRace(){
+				//generates a random whole number between 1 and 6
 				let randomNumber = Math.floor(Math.random() * 6) + 1;
-				document.getElementById('display').innerText = randomNumber;
+				//sends alerts about the race
 				alert("The Race is in progress... Press OK");
 				alert("WOW! Princess has overtaken Horsey, but its not over yet... Press OK")
 		        alert("The Race has ended press OK to view results");	
+		        //records the generated number
+				document.getElementById('display').innerText = randomNumber;
 			}
-		   function pickHorse(){
-		       let usersHorse = prompt("What NUMBER Horse", "Enter a number from 1 - 6");
-		       document.getElementById('choice').innerText = usersHorse;		
-		}
